dfb2ffcb2ed8dc31b6110850d575322fd4a2dd36
Carlos <carlosberrios@protonmail.com>
2023-01-26 20:28:32 -0300

    api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import MiApi from "./components/MiApi";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <div className="header">
+        <h1>Bienvenido</h1>
+        <h1>al Digimundo</h1>    
+      </div>
+      <div className="description">
+        <div>
+        <h2>¿Qué son los Digimon?</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Son formas de vida artificial creadas a partir de bits y piezas perdidas de datos de ordenadores, que habitan 
+        dentro de los aparatos electrónicos que posean memorias no volátiles. Los Digimon son criados por humanos llamados 
+        "Tamers" (niños elegidos en Digimon Adventure y en su secuela Digimon 02) para combatir a los villanos que amenazan 
+        con destruir el equilibrio del Mundo Digital.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src="https://images.fanart.tv/fanart/digimon-digital-monsters-59a9b7f4cf333.png" alt="digivice" />
+        </div>
+        <div>
+        <h2>Características de los Digimon</h2>
+        <p>
+          Los Digimon son las manifestaciones físicas de los datos que actúan como criaturas vivas, y están formados por datos 
+          fragmentados procedentes de la red de ordenadores del mundo real después de que un virus lanzado por un hacker empezase 
+          a absorber información, evolucionando y cambiando. Los Digimon nacen de un Digitama (Digi-Huevo), y eventualmente crecen 
+          durante un proceso conocido como Digievolución, en el cual sufren una serie de cambios físicos, y en el mayor de los casos, 
+          un incremento en sus poderes. Algunos Digimon actúan de forma bestial, guiados por sus instintos salvajes, mientras que otros 
+          poseen inteligencia, sentimientos y personalidad humana. </p>
+        </div>
+      </div>
+      <div className="api-section">
+        <h3>Estimado niño elegido:</h3>
+        <p>Acabas de ser escogido para salvar al mundo digital. En esta sección podrás buscar al digimon que te acompañará en tu camino</p>
+        <MiApi />
+      </div>
+      <div className="footer">
+        <img src="https://vignette.wikia.nocookie.net/digimon/images/a/a0/Agumon_DSLE.gif/revision/latest/top-crop/width/300/height/300?cb=20171223095241" alt="digisaludo" />
+        <p>Gracias por visitar esta página!</p>
+      </div>
     </div>
   );
 }
---
+++ src/components/MiApi.jsx
@@ -0,0 +1,80 @@
+import { useState, useEffect } from "react";
+
+const MiApi = () => {
+    //Guardar info traída desde la Api
+    const [info, setInfo] = useState ([]);
+    //Llamar a la función que consume la Api
+    useEffect (() => {
+        consultarInfo();
+    }, [])
+    //Funcion que consulta la Api
+    const consultarInfo = async () => {
+        const response = await fetch("https://digimon-api.vercel.app/api/digimon");
+        const data = await response.json();
+        setInfo(data);
+    }
+    //Estado de búsqueda
+    const [search, setSearch] = useState ('')
+    //Función de búsqueda
+    const busqueda = (e) => {
+        setSearch(e.target.value)
+    }
+    //Filtrado
+    let results = []
+    if(!search) {
+        results = info
+    } else{
+        results = info.filter((elemento) =>
+        elemento.name.toLowerCase().includes(search.toLocaleLowerCase()) )
+    }
+    //Funcion de sort
+    const [order, setOrder] = useState('ASC');
+    const sorting = (col) => {
+        if(order === 'ASC') {
+            const sorted = [...info].sort((a, b) =>
+            a[col].toLowerCase() > b[col].toLowerCase() ? 1 : -1
+            );
+            setInfo(sorted);
+            setOrder('DSC');
+        }
+        if(order === 'DSC') {
+            const sorted = [...info].sort((a, b) =>
+            a[col].toLowerCase() < b[col].toLowerCase() ? 1 : -1
+            );
+            setInfo(sorted);
+            setOrder('ASC');
+        }
+    }
+
+
+    return (
+        <div>
+            <div className="digi-container">
+                <div>
+                    <input value={search} onChange={busqueda} type="text" placeholder="Busca tu Digimon" className="form-control mt-3" />
+                    <button className="btn btn-sm bg-warning mt-3" onClick={()=>sorting("name")}>Sort: A-Z</button>
+                </div>
+                <table className='table table-bordered  table-hover shadow-lg mt-3'>
+                    <thead>
+                        <tr className='bg-primary text-black'>
+                            <th>Digimon</th>
+                            <th>Foto</th>
+                            <th>Nivel</th>
+                        </tr>
+                    </thead>
+                    <tbody className='bg-secondary text-black'>
+                        { results.map((elemento) => (
+                            <tr key={elemento.id}>
+                                <td>{elemento.name}</td>
+                                <td className="digimon-img"><img src={elemento.img} alt="" /></td>
+                                <td>{elemento.level}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+    export default MiApi;
